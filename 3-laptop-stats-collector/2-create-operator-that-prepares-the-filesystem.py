702569692366f4922c928bbb76df234365fa5851
Denis Dallinga <ddallinga@schubergphilis.com>
2019-04-25 15:26:11 +0200

    First operator for the laptop stats workshop

---
+++ 3-laptop-stats-collector/2-create-operator-that-prepares-the-filesystem.py
@@ -0,0 +1,17 @@
+#Import the bash operator
+from airflow.operators.bash_operator import BashOperator
+
+# Instantiate a Operator. On instance of an operator is called a task
+create_base_directory_task = BashOperator(
+    # The task id should be unique within the
+    task_id='create_base_data_directory',
+    dag=dag,
+    # Some parameters accept jinja templates and will resolve them when being
+    # executed by the scheduler. The bash_command parameter of the BashOperator
+    # is one of them. We use an Airflow Variable to read the base data path and
+    # we partition the directories by the execution hour of the operator.
+    bash_command=(
+        "mkdir -p {{ var.value.base_data_path }}/{{ dag.dag_id }}/"
+        "{{ execution_date.strftime('%Y/%m/%d/%H') }}"
+    )
+)
---
+++ 3-laptop-stats-collector/3-setting-a-variable.md
@@ -0,0 +1,5 @@
+# Setting a varialbe in airflow
+This can be done through the user interface or through the command line:
+```
+pipenv run airflow variables --set base_data_path $AIRFLOW_HOME/data
+```
