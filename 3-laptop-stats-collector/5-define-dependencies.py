d5bb275b7769d2b8c2051810299660425eb1a787
Denis Dallinga <ddallinga@schubergphilis.com>
2019-04-26 11:56:07 +0200

    Dependencies and open chrome tabs operator

---
+++ 3-laptop-stats-collector/5-define-dependencies.py
@@ -0,0 +1,2 @@
+# define dependencies:
+create_base_directory_task >> collect_laptop_load_task
---
+++ 3-laptop-stats-collector/6.create-operator-that-records-open-chrome-tabs.py
@@ -0,0 +1,12 @@
+collect_open_chrome_tabs_command = (
+    "ps -ef | grep Chrome | grep type=renderer | wc -l | sed 's/ //g' > "
+    "{{ var.value.base_data_path }}/{{ dag.dag_id }}/"
+    "{{ execution_date.strftime('%Y/%m/%d/%H') }}/"
+    "open_chrome_tabs_{{ execution_date.strftime('%Y%m%d%H%M') }}.txt"
+)
+
+collect_open_chrome_tabs_task = BashOperator(
+    task_id='collect_open_chrome_tabs',
+    dag=dag,
+    bash_command=collect_open_chrome_tabs_command
+)
